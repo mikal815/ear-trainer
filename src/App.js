0d2b5c2011e1f304c0d4969aaabf9adfc6c9fbf6
Thomas Leis <tjleis25@gmail.com>
2018-10-09 21:11:43 -0400

    Navbar Complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./components/Header";
+import BackgroundImage from "./components/BackgroundImage";
+// import Score from "./components/Score";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header branding="Music To My Ears" />
+        <BackgroundImage />
+        {/* <div className="container">
+          <Score />
+        </div> */}
       </div>
     );
   }
