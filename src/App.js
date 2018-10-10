0c82aad7448be80b5b13979cfcf82090a94092b1
Szsaunders <sadikisaunders@gmail.com>
2018-10-09 21:10:12 -0400

    Keyboard component active.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Keyboard from './components/Keyboard'
+import 'react-piano/dist/styles.css';
 import './App.css';
 
 class App extends Component {
+  state = {
+
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Keyboard />
     );
   }
 }
+
+
 
 export default App;
