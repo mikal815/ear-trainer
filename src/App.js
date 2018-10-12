3d7a39a546c8fe994638af1abb3f50bc8978bf19
baehansung <bae.hansung@gmail.com>
2018-10-11 21:27:04 -0400

    display update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Keyboard from "./components/Keyboard";
 import BackgroundImage from "./components/BackgroundImage";
 import PlayBox from "./components/PlayBox";
+import { Container, Row, Col } from "./components/Grid";
 
 import "react-piano/dist/styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -17,7 +18,13 @@ class App extends Component {
         <Header />
         <BackgroundImage />
         <Keyboard />
-        <PlayBox />
+        <Container>
+          <Row>
+            <Col size="lg-4">
+                <PlayBox />
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
