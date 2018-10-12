8f926cf2dfdf0d32d31071bd6390e83f9d6d1104
Szsaunders <sadikisaunders@gmail.com>
2018-10-11 20:27:29 -0400

    merging from master. really need to figure out how to do that properly

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
-import React, { Component } from 'react';
-import Keyboard from './components/Keyboard'
-import 'react-piano/dist/styles.css';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Keyboard from "./components/Keyboard";
+import BackgroundImage from "./components/BackgroundImage";
+import PlayBox from "./components/PlayBox";
+
+import "react-piano/dist/styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 class App extends Component {
-  state = {
+  state = {};
 
+  shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+
+  toneRowGenerator = () => {
+    const toneArray = [
+      "C3",
+      "D3",
+      "E3",
+      "F3",
+      "G3",
+      "A4",
+      "B4",
+      "C#3",
+      "D#3",
+      "F#3",
+      "G#3",
+      "A#4"
+    ];
+    let tempArray = this.shuffle(toneArray);
+    this.setState({ currentSong: tempArray})
   }
-
 
   render() {
     return (
-      <Keyboard />
+      <div>
+        <Header />
+        <BackgroundImage />
+        <Keyboard />
+        <PlayBox />
+      </div>
     );
   }
 }
-
-
 
 export default App;
--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -1,27 +1,27 @@
 import React from "react";
 import Tone from "tone";
-import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
+import { Piano, KeyboardShortcuts, MidiNumbers } from "react-piano";
 
 function Keyboard() {
-  const firstNote = MidiNumbers.fromNote('c3');
-  const lastNote = MidiNumbers.fromNote('f5');
+  const firstNote = MidiNumbers.fromNote("c3");
+  const lastNote = MidiNumbers.fromNote("f5");
   const keyboardShortcuts = KeyboardShortcuts.create({
     firstNote: firstNote,
     lastNote: lastNote,
-    keyboardConfig: KeyboardShortcuts.HOME_ROW,
+    keyboardConfig: KeyboardShortcuts.HOME_ROW
   });
-  
-  var synth = new Tone.Synth().toMaster()
- 
+
+  var synth = new Tone.Synth().toMaster();
+
   return (
     <Piano
       noteRange={{ first: firstNote, last: lastNote }}
-      playNote={(midiNumber) => {
-        var x = Tone.Frequency(midiNumber, "midi").toNote()
+      playNote={midiNumber => {
+        var x = Tone.Frequency(midiNumber, "midi").toNote();
         synth.triggerAttack(x);
-        // Play a given note - see notes below
+        // Play a given note - e notes below
       }}
-      stopNote={(midiNumber) => {
+      stopNote={midiNumber => {
         synth.triggerRelease();
       }}
       width={1000}
