1ae69c2538d3f82b6d5eab90cc8379ea8c642fe7
Thomas Leis <tjleis25@gmail.com>
2018-10-13 13:30:17 -0400

    music to my ears

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -2,35 +2,35 @@ import React from "react";
 import Tone from "tone";
 import { Piano, KeyboardShortcuts, MidiNumbers } from "react-piano";
 
+const firstNote = MidiNumbers.fromNote("c3");
+const lastNote = MidiNumbers.fromNote("c5");
+const keyboardShortcuts = KeyboardShortcuts.create({
+  firstNote: firstNote,
+  lastNote: lastNote,
+  keyboardConfig: KeyboardShortcuts.HOME_ROW
+});
 
+var synth = new Tone.Synth().toMaster();
 
-  const firstNote = MidiNumbers.fromNote("c3");
-  const lastNote = MidiNumbers.fromNote("c5");
-  const keyboardShortcuts = KeyboardShortcuts.create({
-    firstNote: firstNote,
-    lastNote: lastNote,
-    keyboardConfig: KeyboardShortcuts.HOME_ROW
-  });
-
-  var synth = new Tone.Synth().toMaster();
-
-  const Keyboard = props => (
+const Keyboard = props => (
+  <div className="container" style={{ width: "100%", height: "auto" }}>
     <Piano
       noteRange={{ first: firstNote, last: lastNote }}
       playNote={midiNumber => {
         var x = Tone.Frequency(midiNumber, "midi").toNote();
         synth.triggerAttack(x);
       }}
-      onPlayNoteInput={(midiNumber) => {
+      onPlayNoteInput={midiNumber => {
         var x = Tone.Frequency(midiNumber, "midi").toNote();
-         props.scorekeeper(x) }}
+        props.scorekeeper(x);
+      }}
       stopNote={midiNumber => {
         synth.triggerRelease();
       }}
       width={1345}
       keyboardShortcuts={keyboardShortcuts}
     />
-  );
-
+  </div>
+);
 
 export default Keyboard;
