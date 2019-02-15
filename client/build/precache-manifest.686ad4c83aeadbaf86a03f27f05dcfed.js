75a006fd3a03da2dd3fe46c2f791cbd249ee32d6
Szsaunders <sadikisaunders@gmail.com>
2019-02-14 19:40:25 -0500

    new build dir

---
+++ client/build/precache-manifest.686ad4c83aeadbaf86a03f27f05dcfed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c69ab2919fafe37be77847fff0c5d12",
+    "url": "/static/media/soundSystem.0c69ab29.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f699b3f61ec73da4c575",
+    "url": "/static/js/main.f699b3f6.chunk.js"
+  },
+  {
+    "revision": "a9c797c8ebce5cd52418",
+    "url": "/static/js/1.a9c797c8.chunk.js"
+  },
+  {
+    "revision": "f699b3f61ec73da4c575",
+    "url": "/static/css/main.7b56dcfb.chunk.css"
+  },
+  {
+    "revision": "a9c797c8ebce5cd52418",
+    "url": "/static/css/1.34383381.chunk.css"
+  },
+  {
+    "revision": "55c20c09bb4245924d5dbf2709a653fa",
+    "url": "/index.html"
+  }
+];
